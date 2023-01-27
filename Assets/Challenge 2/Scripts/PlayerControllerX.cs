8d0e50161e69b2d8c81ad632904dc26c97a5c569
dav <usr21p1@gmail.com>
2023-01-27 12:34:28 -0800

    done.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,28 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float timePassed = 0;
+    private bool canSpawn = true;
+
     // Update is called once per frame
     void Update()
     {
+        if (!canSpawn)
+        {
+            timePassed += Time.deltaTime;
+        }
+
+        if (timePassed >= 0.5f)
+        {
+            canSpawn = true;
+            timePassed = 0.0f;
+        }
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawn = false;
         }
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,22 +11,24 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        var randomSpawnInterval = Random.Range(3.0f, 6.0f);
+        InvokeRepeating("SpawnRandomBall", startDelay, randomSpawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        var randomSpawnIndex = Random.Range(0, ballPrefabs.Length);
+        var randomBallPrefab = ballPrefabs[randomSpawnIndex];
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(randomBallPrefab, spawnPos, randomBallPrefab.transform.rotation);
     }
 
 }
